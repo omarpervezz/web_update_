8313d80114e122c76cf6059b9687c97204ecf745
Omar Pervez <70445442+omarpervezz@users.noreply.github.com>
2023-02-17 23:23:47 +0600

    I have done the work

--- main.js
+++ main.js
@@ -168,6 +168,30 @@ $(document).ready(function () {
       $("html, body").animate({ scrollTop: 0 }, "300");
     });
   });
+
+  $(function () {
+    $("a[href*=#]:not([href=#])").click(function () {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
 });
 
 // Get the current page URL
